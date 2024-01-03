5cd16d8b1cbcbc0d692fc43ea23b58e837901da5
Pouya Nikzadaghdas <puyaniek@gmail.com>
2024-01-03 01:05:19 +0100

    To have information in our database, we store several products in the database.

---
+++ inventory-service/src/main/java/com/pouya/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,38 @@
+package com.pouya.inventoryservice;
+
+import com.pouya.inventoryservice.entities.Inventory;
+import com.pouya.inventoryservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.UUID;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+
+	@Bean
+	public CommandLineRunner localData (InventoryRepository inventoryRepository)
+	{
+		return  args -> {
+			Inventory inventory = new Inventory() ;
+			inventory.setQuantity(100);
+			inventory.setSkuCode(UUID.randomUUID().toString());
+			inventoryRepository.save(inventory) ;
+
+
+			Inventory inventory1 = new Inventory() ;
+			inventory1.setQuantity(0);
+			inventory1.setSkuCode(UUID.randomUUID().toString());
+			inventoryRepository.save(inventory1) ;
+
+		} ;
+	}
+
+}
