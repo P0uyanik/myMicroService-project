118a0f7f8b1e692d2ff03db4606736b3be59b63d
Pouya Nikzadaghdas <puyaniek@gmail.com>
2024-01-04 14:18:42 +0100

    The alternative endpoint, instead of taking a single SKU code, accepts a list of these codes and checks them

--- inventory-service/src/main/java/com/pouya/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/pouya/inventoryservice/controller/InventoryController.java
@@ -1,21 +1,23 @@
 package com.pouya.inventoryservice.controller;
 
+import com.pouya.inventoryservice.dto.InventoryResponse;
 import com.pouya.inventoryservice.service.InventoryServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/inventory")
 @RequiredArgsConstructor
 public class InventoryController {
-private final InventoryServiceImpl inventoryService ;
+    private final InventoryServiceImpl inventoryService;
 
-    @GetMapping("/{sku-code}")
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public  boolean isInStock(@PathVariable ("sku-code") String skuCode)
-    {
-       return inventoryService.isInStock(skuCode) ;
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
+        return inventoryService.isInStock(skuCode);
     }
 
 }
