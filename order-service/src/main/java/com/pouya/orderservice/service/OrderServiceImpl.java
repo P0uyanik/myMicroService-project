4031e71deb1dcf979cb360e63f64085c273fbd54
Pouya Nikzadaghdas <puyaniek@gmail.com>
2024-01-04 14:17:00 +0100

    We want to check the inventory using the orderservice. For this, we use WebFlux and inject it into an endpoint that provides us with the availability status through the SKU code.

--- order-service/src/main/java/com/pouya/orderservice/service/OrderServiceImpl.java
+++ order-service/src/main/java/com/pouya/orderservice/service/OrderServiceImpl.java
@@ -1,23 +1,41 @@
 package com.pouya.orderservice.service;
 
+import com.pouya.orderservice.dto.InventoryResponse;
 import com.pouya.orderservice.entities.Order;
 import com.pouya.orderservice.entities.OrderLineItems;
+import com.pouya.orderservice.exceptions.ExceptionsHandlerClass.CheckingCorrectnessOfResult;
 import com.pouya.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
-public class OrderServiceImpl implements  OrderService {
-    private final OrderRepository orderRepository ;
+public class OrderServiceImpl implements OrderService {
+    private final OrderRepository orderRepository;
+    private final WebClient webClient;
+
     @Override
     public void placeOrder(List<OrderLineItems> orderAsList) {
+
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
-        order.setOrderLineItems(orderAsList) ;
-        orderRepository.save(order) ;
+        order.setOrderLineItems(orderAsList);
+        Stream<String> skuCodes = order.getOrderLineItems().stream().map(OrderLineItems::getSkuCode);
+        InventoryResponse[] inventoryResponses = webClient.get().uri("http://localhost:8082/api/inventory"
+                        , uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class).block();
+
+
+        boolean result = Arrays.stream(inventoryResponses).allMatch(InventoryResponse::isInStock);
+        CheckingCorrectnessOfResult.availabilityOfProduct(result);
+
+        orderRepository.save(order);
     }
 }
